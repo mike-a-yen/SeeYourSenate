8efaf938c8c85695da588022370a758111b3aa87
mike-a-yen <mike.a.yen@gmail.com>
2016-10-30 17:18:57 -0700

    mpld3 is diff

--- app/cluster.py
+++ app/cluster.py
@@ -2,7 +2,8 @@ import numpy as np
 
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt, mpld3
+import matplotlib.pyplot as plt
+import mpld3
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
