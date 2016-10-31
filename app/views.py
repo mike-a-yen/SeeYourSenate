5480f5f1241dad7034a625bbb70291ec871749fd
mike-a-yen <mike.a.yen@gmail.com>
2016-10-30 17:30:16 -0700

    finish staticness of fifgs

--- app/views.py
+++ app/views.py
@@ -7,7 +7,6 @@ from app.cluster import plot_2d, plot_3d
 @app.route('/')
 @app.route('/index')
 def index():
-    plot_2d(xy,clusters,votes)
     cluster_plot = open('app/static/img/cluster.html','r').read()
     return flask.render_template('index.html',
                                  cluster_plot=cluster_plot)
