4ce265850e62214369337ecc2f02eb456e2f1e4d
mike-a-yen <mike.a.yen@gmail.com>
2016-11-04 14:18:06 -0700

    finalist

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
+
 
 app = Flask(__name__)
 
+local_db = 'sqlite:////Users/mayen/Programming/WillItPass/tmp/congress.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
+                                                       local_db)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+print('DB_URI :',app.config['SQLALCHEMY_DATABASE_URI'])
+app.config['SECRET_KEY'] = 'secretsarenofun'
+db = SQLAlchemy(app)
+
 from app import views
+from app.models import *
