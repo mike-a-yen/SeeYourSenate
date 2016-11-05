45167990de7c4415e0de3b265af179223ccf8c0f
mike-a-yen <mike.a.yen@gmail.com>
2016-11-05 13:11:24 -0700

    redis queue

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+from rq import Queue
+from rq.job import Job
+from worker import conn
+
 
 app = Flask(__name__)
 
@@ -13,12 +17,16 @@ print('DB_URI :',app.config['SQLALCHEMY_DATABASE_URI'])
 app.config['SECRET_KEY'] = 'secretsarenofun'
 db = SQLAlchemy(app)
 
+q = Queue(connection=conn)
+
 from app import views
 from app.models import *
 from app.build_models import build_models
 
 print('#'*50)
 print('Building member models')
-build_models()
-print('Done building models')
+job = q.enqueue_call(func=build_models,
+                     result_ttl=5000)
+print('RQ ID :',job.get_id())
+print('Build job set in queue')
 print('#'*50)
