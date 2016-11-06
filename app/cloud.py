3d2496c2866564233f7aacf396b7f94517625e89
mike-a-yen <mike.a.yen@gmail.com>
2016-11-06 12:06:40 -0800

    senator pieces

---
+++ app/cloud.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+from PIL import Image
+from scipy.misc import imread
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+from palettable.colorbrewer.sequential import Reds_9
+from palettable.colorbrewer.sequential import Greens_9
+
+from wordcloud import WordCloud, STOPWORDS
+
+
+def green_color_func(word, font_size, position, orientation,
+                   random_state=None, **kwargs):
+    return tuple(Greens_9.colors[np.random.randint(3,9)])
+
+def red_color_func(word, font_size, position, orientation,
+                   random_state=None, **kwargs):
+    return tuple(Reds_9.colors[np.random.randint(3,9)])
+
+def cloud_to_fig(cloud):
+    fig,ax = plt.subplots(figsize=(5,5))
+    ax.imshow(cloud,origin='lower')
+    return fig
+    
+def make_word_cloud(word_freq, type='Yea'):
+    """Make a word cloud from a list of words"""
+    if type =='Yea':
+        thumb = Image.open('app/static/img/thumbs-up.png')
+        color_func = green_color_func
+    elif type == 'Nay':
+        thumb = Image.open('app/static/img/thumbs-down.png')
+        color_func = red_color_func
+    else:
+        thumb = Image.open('app/static/img/thumbs-up.png')
+        color_func = green_color_func
+        
+    mask = Image.new("RGB", thumb.size, (255,255,255))
+    mask.paste(thumb,thumb)
+    mask = np.array(mask)
+    
+    
+    wc = WordCloud(background_color='white',
+                   max_words=80,
+                   mask=mask,
+                   stopwords=STOPWORDS)
+    #wc = wc.generate_from_frequencies(word_freq)
+    wc = wc.generate(' '.join(word_freq.keys()))
+    wc = wc.recolor(color_func=color_func)
+    fig = cloud_to_fig(wc)
+    return fig
---
+++ app/member_topics.py
@@ -0,0 +1,70 @@
+from app import db
+from app.models import *
+
+from app.build_models import (get_decision_text,
+                              get_vote_history,
+                              vote_map)
+from app.utils import merge_dicts
+
+import pandas as pd
+import numpy as np
+import re
+import json
+import pickle
+
+from sklearn.decomposition import TruncatedSVD, PCA
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+def load_vectorizer(memid):
+    member = db.session.query(Member).filter_by(id=memid).first()
+    path = member.vectorizer_path
+    return pickle.load(open(path,'rb'))
+
+def extract_words_from_clusters(features, centroids, n_words=6):
+    num_centroids = len(centroids)
+    order_centroids = np.argsort(centroids)[:,::-1]
+
+    topics = set()
+    for i in range(num_centroids):
+        words = [[features[ind]] for ind in order_centroids[i,:n_words]]
+        topics = topics.union(*map(set,words))
+    return topics
+
+def get_word_freq_from_centroid(centroid, features, n):
+    """Given the centroid and features return the
+    top n words with their tfidf
+    """
+    ordered_centroid = centroid.argsort()[::-1]
+    word_freq = {features[ind]:centroid[ind] \
+                 for ind in ordered_centroid[:n]}
+    return word_freq
+
+def member_vote_table(memid):
+    text = get_decision_text(memid)
+    df = pd.DataFrame(text,columns=['title','question','subject'])
+    df['vote'] = get_vote_history(memid)
+    df['body'] = df['title'].str.cat(df['question'],sep=' ')\
+                                .str.cat(df['subject'],sep=' ')
+    df['body'] = df['body'].apply(lambda x: re.sub("\d+", "", x))
+    df['result'] = df['vote'].apply(vote_map)
+    return df
+
+def vote_topic_freq(memid):
+    df = member_vote_table(memid)
+    vectorizer = load_vectorizer(memid)
+    features = vectorizer.get_feature_names()
+    votes = df['vote'].unique()
+    topic_words = {vote:set() for vote in votes}
+    for vote in votes:
+        sub = df[df['vote']==vote]
+        matrix = vectorizer.fit_transform(sub['body'])
+        kmeans = KMeans( min(8,len(sub)) )
+        kmeans.fit_transform(matrix)
+        sub['cluster'] = kmeans.labels_.tolist()
+        topic_freq = [get_word_freq_from_centroid(centroid, features, 10)\
+                      for centroid in kmeans.cluster_centers_]
+        topic_words[vote] = merge_dicts(topic_freq)
+    return topic_words
