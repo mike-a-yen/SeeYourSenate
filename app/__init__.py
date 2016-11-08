3bd5dc784282104f579b762b855a63e6033a5199
mike-a-yen <mike.a.yen@gmail.com>
2016-11-08 08:47:44 -0800

    restart

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 
-from rq import Queue
-from rq.job import Job
-from worker import conn
+#from rq import Queue
+#from rq.job import Job
+#from worker import conn
 
 
 app = Flask(__name__)
@@ -18,17 +18,17 @@ print('DB_URI :',app.config['SQLALCHEMY_DATABASE_URI'])
 app.config['SECRET_KEY'] = 'secretsarenofun'
 db = SQLAlchemy(app)
 
-q = Queue(connection=conn)
+#q = Queue(connection=conn)
 
 from app import views
 from app.models import *
 from app.build_models import build_models
 
-print('#'*50)
-print('Building member models')
-job = q.enqueue(build_models,
-                timeout=1200,
-                result_ttl=5000)
-print('RQ ID :',job.get_id())
-print('Build job set in queue')
-print('#'*50)
+#print('#'*50)
+#print('Building member models')
+#job = q.enqueue(build_models,
+#                timeout=1200,
+#                result_ttl=5000)
+#print('RQ ID :',job.get_id())
+#print('Build job set in queue')
+#print('#'*50)
