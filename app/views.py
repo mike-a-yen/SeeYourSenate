05b43932eeb18707ef4ce07cd05075fce3eb50f9
mike-a-yen <mike.a.yen@gmail.com>
2016-12-20 17:35:40 -0800

    bills close up page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app, db, BASE_DIR
 from app.models import *
 from app.member_topics import vote_topic_freq
+from app.member_utils import vote_map
 from app.build_models import positive_negative_subjects, voting_subject_record
 from app.model_prediction import senate_prediction
 from app.utils import (get_random_member,
@@ -10,7 +11,7 @@ from app.cloud import make_word_cloud, save_member_cloud
 
 import os
 import flask
-
+import pandas as pd
 import numpy as np
 import pickle
 
@@ -64,3 +65,37 @@ def active():
                                     'rb'))
     return flask.render_template('active_bills.html',
                                  active_bills=active_bills)
+
+@app.route('/bill')
+def bill():
+    bill_id = flask.request.args.get('bill_id')
+    id = bill_id.lower()+'-114'
+
+    print('bill',id)
+    print(bill_id)
+    billquery = db.session.query(Bill).filter_by(bill_id=id)
+    bill = billquery.first()
+
+    session = db.session.query(Session)\
+              .filter(Session.bill_id==id)\
+              .order_by(Session.number.desc())\
+              .order_by(Session.date.desc()).first()
+    
+    votes = db.session.query(Member.first_name,Member.last_name,Member.state,Member.party,
+                             MemberSession.vote)\
+                             .filter(Member.member_id==MemberSession.member_id)\
+                             .filter(MemberSession.session_id==session.session_id)\
+                             .filter(Session.number==session.number)\
+                             .filter(Session.bill_id==id)\
+                             .order_by(Member.state.asc(),
+                                       Member.party.asc(),
+                                       Member.last_name.asc()).all()
+    
+    vote_record = pd.DataFrame(votes,columns=['first_name','last_name','state','party','vote'])
+    vote_record['vote'] = vote_record['vote'].apply(vote_map)
+    vote_record = vote_record.T.to_dict().values()
+    vote_record = sorted(vote_record,key=lambda x:(x['state'],x['party'],x['last_name']))
+    return flask.render_template('bill.html',
+                                 display_title=bill_id,
+                                 bill=bill,
+                                 votes=vote_record)
